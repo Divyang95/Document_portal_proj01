cb2f60f226731d57ddff9b819a33af03e12ed73a
Divyang95 <divyang.thakkar.9231@gmail.com>
2025-08-05 18:14:14 +0530

    document analyser updated

---
+++ prompt/prompt_library.py
@@ -0,0 +1,14 @@
+#Prepare prompt template 
+from langchain_core.prompts import ChatPromptTemplate 
+
+prompt = ChatPromptTemplate.from_template(
+"""
+You are a highly capable assistant trained to analyze and summarize documents.Return ONLY
+valid JSON matching the exact schema below.
+
+{format_instructions}
+
+Analyze the document:
+{document_text}
+            
+""")
